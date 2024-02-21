440edf15728c23724c8692ffa122354fc934b1dd
Nishant <nishant@tensorbule.com>
2024-02-21 12:05:23 +0530

    Initial Commit

---
+++ TestingOpen/TestingOpenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingOpenApp.swift
+//  TestingOpen
+//
+//  Created by Nishant Minerva on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingOpenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
